d7cbfabf4bad78a9c3e8573c57cc44ae8799f546
Muhammad Salman Amir <mmsa155j3@gmail.com>
2026-04-05 00:45:21 +0500

    flway bug fixed

--- backend/auth/src/main/resources/db/migration/V2__triggers.sql
+++
@@ -1,8 +0,0 @@
-CREATE TRIGGER before_user_insert
-BEFORE INSERT ON users
-FOR EACH ROW
-IF NEW.username NOT REGEXP '^[A-Za-z][A-Za-z0-9_]{2,49}$'
-    OR NEW.email NOT REGEXP '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$'
-THEN
-    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Invalid insert format';
-END IF;
--- backend/auth/src/main/resources/db/migration/V3__triggers3.sql
+++
@@ -1,8 +0,0 @@
-CREATE TRIGGER before_user_update
-BEFORE UPDATE ON users
-FOR EACH ROW
-IF (NEW.username <> OLD.username AND NEW.username NOT REGEXP '^[A-Za-z][A-Za-z0-9_]{2,49}$')
-    OR (NEW.email <> OLD.email AND NEW.email NOT REGEXP '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$')
-THEN
-    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Invalid update format';
-END IF;
