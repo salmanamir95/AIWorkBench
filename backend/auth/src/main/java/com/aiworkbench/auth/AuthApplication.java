116ace165a16165f19972760495cdbebbcec49a0
salmanamir95 <mmsa155j3@gmail.com>
2026-07-02 19:05:42 +0500

    revamp

--- backend/auth/src/main/java/com/aiworkbench/auth/AuthApplication.java
+++ backend/auth/src/main/java/com/aiworkbench/auth/AuthApplication.java
@@ -3,9 +3,11 @@ package com.aiworkbench.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableWebFluxSecurity
 public class AuthApplication {
 
 	public static void main(String[] args) {
--- backend/auth/src/main/java/com/aiworkbench/auth/dtos/AuthProvisioningDto.java
+++
@@ -1,3 +0,0 @@
-package com.aiworkbench.auth.dtos;
-
-public record AuthProvisioningDto(Long userId, String email, String password) {}
